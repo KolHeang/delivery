43f56a368b35e699683ee0861ccefaf336af9f46
kolheang <kolheang777@gmail.com>
2026-06-17 01:47:43 +0000

    fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['10.1.52.220'],
+  allowedDevOrigins: ['10.1.52.220','127.0.0.1'],
 };
 
 export default nextConfig;
